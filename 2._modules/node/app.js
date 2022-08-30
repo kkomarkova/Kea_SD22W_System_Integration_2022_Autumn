81eb1c75cb4a7984a3bc634b164d4769df5ab3b4
Anders Latif <andl@kea.dk>
2022-08-30 09:01:42 +0200

    Finished implementing modules in Node.js and frontend Javascript

--- 2._modules/node/app.js
+++ 2._modules/node/app.js
@@ -1,5 +1,13 @@
-const express = require("express");
+import express from 'express';
 const app = express();
+import path from 'path';
+
+app.use(express.static("public"));
+
+
+app.get("/duck", (req, res) => {
+    res.sendFile(path.resolve("./public/duck.html"));
+});
 
 app.get("/endpoint", (req, res) => {
     res.send({ message: "OK OK OK" });
---
+++ 2._modules/node/public/duckFactoryLib.js
@@ -0,0 +1,5 @@
+export const ducks = ["Donald", "Ronald", "Santa Clause"];
+
+export function countDucks() {
+    return ducks.length;
+}
