e52a725e3082cf187a3526d08382767e8fc06cfc
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-10-03 22:16:14 +0200

    changes

--- Dateservers-part1/server1.py
+++ Dateservers-part1/server1.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from datetime import datetime
 
@@ -14,6 +15,23 @@ def read_root():
     return {'ISO DateTime:', iso_date}
 
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Date API documentation",
+        version="2.5.0",
+        description="This is a very custom Date API documentation schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Union[str, None] = None):
 #     return {"item_id": item_id, "q": q}
--- Dateservers-part1/server2.js
+++ Dateservers-part1/server2.js
@@ -1,36 +1,71 @@
-import fetch from 'node-fetch';
-import express, { response } from 'express';
-
-// const express = require('express')
+import express from 'express'
+import yaml from 'js-yaml'
+import * as fs from 'fs';
 const app = express()
-const port = 6000
 
 
+const path_to_file_template = '../1._file_types/me.'
+
+//Getting the current date
 app.get('/', (req, res) => {
- var date = new Date().toISOString();
-  res.send("The current date is "  + date)
+  var date = new Date().toISOString();
+   res.send("The current date is "  + date)
+ })
+ 
+//YAML
+app.get("/yml", async (req, res) => {
+    const yml_data = await fs.readFile(path_to_file_template + "yaml", {encoding: 'utf8'});
+    const doc = yaml.load(yml_data);
+    res.json(doc)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//JSON
+app.get("/json", async (req, res) => {
+  const jsonFile = await fs.readFile(path_to_file_template+'json', {encoding: 'utf8'});
+  res.json(jsonFile)
 })
 
-//Trying to fetch google pages - DONE
-// fetch('https://google.com')
-// .then(res => res.text())
-// .then(text => console.log(text));
+//CSV
+// app.get("/csv", async (req, res) => {
+//   const file = req.file
+
+//   const data = fs.readFileSync(path_to_file_template+'csv')
+//   parse(data, (err, records) => {
+//     if (err) {
+//       console.error(err)
+//       return res.status(400).json({success: false, message: 'An error occurred'})
+//     }
+
+//     return res.json({data: records})
+//   })
+// })
+
+//XML
+app.get('/xml', function (req, res) {
+  res.set('Content-Type', 'text/xml');
+  res.send(fs.readFileSync(path_to_file_template+'xml', {encoding: 'utf-8'}))
+})
+//TEXT
+app.get("/txt", async (req, res) => {
+  const txtFile = await fs.readFile(path_to_file_template+'txt', {encoding: 'utf8'});
+  res.json(txtFile)
+})
+
 
 // JSON - I am running the file on local host with using command npx http-server 
-fetch('http://192.168.56.1:8080/me.json')
-.then(response => {
-   return response.json();
+// fetch("/" + path_to_file_template + "json" )
+// .then(response => {
+// return response.json()});
    
+// })
+// .then(jsondata => console.log(jsondata));
+
+// app.get("/txt", async (req, res) => {
+//     const txtFile = await fs.readFile(path_to_file_template+'txt', {encoding: 'utf8'});
+//     const parsedTxtFileFromJson = txtToJson(txtFile)
+//     res.json(parsedTxtFileFromJson)
+// })
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port 3000`)
 })
-.then(jsondata => console.log(jsondata));
-
-//Read Text file 
-fetch('http://192.168.56.1:8080/me')
-.then(res => res.text())
-.then(text => console.log(text));
-
-
